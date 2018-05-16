5373b8d745bc3305de727c89084495135e5b20d5
許當麻 <no2don@outlook.com>
2018-05-16 13:02:49 +0800

    加入專案檔案。

---
+++ YoTop/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace YoTop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnChangeUrl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnHome = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnAbout = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnHideBorder = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 28);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(760, 661);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnChangeUrl,
+            this.btnHome,
+            this.btnAbout,
+            this.btnHideBorder});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 626);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 17, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(760, 35);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.DoubleClick += new System.EventHandler(this.statusStrip1_DoubleClick);
+            // 
+            // btnChangeUrl
+            // 
+            this.btnChangeUrl.Name = "btnChangeUrl";
+            this.btnChangeUrl.Size = new System.Drawing.Size(128, 30);
+            this.btnChangeUrl.Text = " - GOTO - ";
+            this.btnChangeUrl.Click += new System.EventHandler(this.btnChangeUrl_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(131, 30);
+            this.btnHome.Text = " - HOME - ";
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(140, 30);
+            this.btnAbout.Text = " - ABOUT - ";
+            this.btnAbout.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // btnHideBorder
+            // 
+            this.btnHideBorder.Name = "btnHideBorder";
+            this.btnHideBorder.Size = new System.Drawing.Size(97, 30);
+            this.btnHideBorder.Text = " - PIN - ";
+            this.btnHideBorder.Click += new System.EventHandler(this.btnHideBorder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 34F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 661);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Font = new System.Drawing.Font("Georgia", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "上班伴侶 for Youtube";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel btnChangeUrl;
+        private System.Windows.Forms.ToolStripStatusLabel btnHome;
+        private System.Windows.Forms.ToolStripStatusLabel btnAbout;
+        private System.Windows.Forms.ToolStripStatusLabel btnHideBorder;
+    }
+}
+
---
+++ YoTop/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoTop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(AppDomain.CurrentDomain.BaseDirectory + "last.txt"))
+            {
+                webBrowser1.Navigate(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "last.txt"));
+            }
+            else {
+                webBrowser1.Navigate("https://www.youtube.com/watch?v=21lu5zktQiQ");
+            }
+           
+
+        }
+
+        public static DialogResult InputBox(string title, string promptText, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            value = textBox.Text;
+            return dialogResult;
+        }
+
+        double op = 1;
+        bool isAdd = false;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            const Int32 WM_NCLBUTTONDBLCLK = 163;
+
+            if (m.Msg == WM_NCLBUTTONDBLCLK)
+            {
+                if (!isAdd)
+                {
+                    op -= 0.2;
+
+                    if (op <= 0.2)
+                    {
+                        op = 0.2;
+                        isAdd = true;
+                    }
+                }
+                else
+                {
+                    op += 0.2;
+
+                    if (op >= 1)
+                    {
+                        op = 1;
+                        isAdd = false;
+                    }
+                }
+
+                this.Opacity = op;
+            }
+
+        }
+
+        private void btnChangeUrl_Click(object sender, EventArgs e)
+        {
+
+            // Source reference : https://dotblogs.com.tw/aquarius6913/2014/09/03/146444
+            this.TopMost = false;
+            string strPath = "https://www.youtube.com/watch?v=21lu5zktQiQ";
+            var result = InputBox("https://www.youtube.com/watch?v=21lu5zktQiQ", "The Site you wanna go :", ref strPath);
+
+            if (result == DialogResult.OK)
+            {
+                webBrowser1.Navigate(strPath);
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = true;
+            }
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=21lu5zktQiQ");
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fun toy from Donma Hsu.\r\n welcome visit my github : https://github.com/donma");
+        }
+
+        private void btnHideBorder_Click(object sender, EventArgs e)
+        {
+            if (this.FormBorderStyle != FormBorderStyle.None)
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                btnHideBorder.Text = "-MOVABLE-";
+            }
+            else {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                btnHideBorder.Text = "-PIN-";
+            }
+        }
+
+   
+
+        private void statusStrip1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.FormBorderStyle != FormBorderStyle.None)
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                btnHideBorder.Text = "-MOVABLE-";
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                btnHideBorder.Text = "-PIN-";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "last.txt", webBrowser1.Url.AbsoluteUri);
+        }
+    }
+}
